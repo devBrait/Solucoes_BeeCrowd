ea4a91600a6650c265127876f07543676235cf66
Vinícius Brait <lorimiervb@gmail.com>
2024-02-10 12:17:23 -0300

    Adição de soluções c e python

---
+++ C/Iniciante/1001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    int A;
+    int B;
+ 
+    printf("");
+    scanf("%d", &A);
+    printf("");
+    scanf("%d", &B);
+    int soma = A + B;
+    printf("X = %i\n", soma);
+    return 0;
+}
