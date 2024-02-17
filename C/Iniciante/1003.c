4ecbe4d8b0aa51126f8003f45f71f566f22681d0
Vinícius Brait <lorimiervb@gmail.com>
2024-02-17 12:42:14 -0300

    Soluções c

---
+++ C/Iniciante/1003.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a, b, soma;
+    
+    printf("");
+    scanf("%d", &a);
+    printf("");
+    scanf("%d", &b);
+    soma = a + b;
+    printf("SOMA = %d\n", soma);
+ 
+    return 0;
+}
---
+++ C/Iniciante/1004.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a, b, prod;
+        
+    printf("");
+    scanf("%d", &a);
+    printf("");
+    scanf("%d", &b);
+    prod = a * b;
+    printf("PROD = %d\n", prod);
+ 
+    return 0;
+}
