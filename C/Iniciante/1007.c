853f80abb3d76f2f1e0f86367061beeb33d0fef6
Vinícius Brait <lorimiervb@gmail.com>
2024-02-21 10:44:59 -0300

    Subindo soluções c

---
+++ C/Iniciante/1007.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a, b, c, d, diferenca;
+    
+    printf("");
+    scanf("%d", &a);
+    printf("");
+    scanf("%d", &b);
+    printf("");
+    scanf("%d", &c);
+    printf("");
+    scanf("%d", &d);
+    
+    diferenca = (a * b - c * d);
+    printf("DIFERENCA = %d\n", diferenca);
+ 
+    return 0;
+}
