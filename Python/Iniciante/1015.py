76e73c1da601ac83a19a05ebed8f8cf59c349899
Vinícius Brait <lorimiervb@gmail.com>
2024-03-23 02:54:19 -0300

    testando arquivo .py

--- Python/Iniciante/1015.py
+++ Python/Iniciante/1015.py
@@ -3,6 +3,6 @@ import math
 x1, y1 = map(float, input().split())
 x2, y2 = map(float, input().split())
 
-distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+distancia = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-print("{:.4f}".format(distance))
+print("{:.4f}".format(distancia))
