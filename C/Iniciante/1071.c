907bc85c1461e150cd506664724593cf76b234df
Vinícius Brait <lorimiervb@gmail.com>
2024-03-28 12:36:32 -0300

    solução 1071.c

---
+++ C/Iniciante/1071.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y, sum = 0;
+
+    scanf("%d %d", &X, &Y);
+
+    if (X > Y) {
+        int temp = X;
+        X = Y;
+        Y = temp;
+    }
+
+    for (int i = X + 1; i < Y; i++) {
+        if (i % 2 != 0) {
+            sum += i;
+        }
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
