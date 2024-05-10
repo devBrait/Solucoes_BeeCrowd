ae785e536bfbfc8bb32337c90b8acdd5d312d8fa
Vinícius Brait <lorimiervb@gmail.com>
2024-05-10 09:38:47 -0300

    add: 1133 and 1143.c solutions

---
+++ C/Iniciante/1133.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    
+    int x, y;
+    
+    scanf("%d", &x);
+    scanf("%d", &y);
+    
+    if(x > y)
+    {
+        for(int i = y+1; i <= x-1; i++)
+        {
+            if(i % 5 == 2 || i % 5 == 3)
+            {
+                printf("%d\n", i);
+            }
+        }
+    }
+    else
+    {
+        for(int i = x+1; i <= y-1; i++)
+        {
+             if(i % 5 == 2 || i % 5 == 3)
+            {
+                printf("%d\n", i);
+            }
+        }
+    }
+}
