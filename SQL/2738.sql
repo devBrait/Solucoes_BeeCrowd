350d244b39a262ac1a97b46ed13e13b9451aaa9a
Vinícius Brait <lorimiervb@gmail.com>
2024-06-26 16:34:13 -0300

    add: 2738.sql solution

---
+++ SQL/2738.sql
@@ -0,0 +1,9 @@
+SELECT 
+    c.name,
+    ROUND(((s.math * 2) + (s.specific * 3) + (s.project_plan * 5)) / 10, 2) AS avg
+FROM 
+    candidate c
+JOIN 
+    score s ON c.id = s.candidate_id
+ORDER BY 
+    avg DESC;
