dc76b1168fd6cd25ea584a002cf427b37f465401
Vinícius Brait <121905548+devBrait@users.noreply.github.com>
2024-08-09 19:51:51 +0000

    add: 1186.c solution

---
+++ C/Iniciante/1186.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    char operation;
+    double m[12][12], sum = 0.0;
+    int count = 0;
+
+    scanf("%c", &operation);
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            scanf("%lf", &m[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            if (i + j >= 12) { 
+                sum += m[i][j];
+                count++;
+            }
+        }
+    }
+
+    if (operation == 'M') {
+        sum /= count;
+    }
+
+    printf("%.1lf\n", sum);
+
+    return 0;
+}
