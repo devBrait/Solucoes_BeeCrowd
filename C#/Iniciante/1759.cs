f55e104667f9efd0f56b3adcec763b024e9bb5bd
Vinícius Brait <lorimiervb@gmail.com>
2024-11-19 16:19:24 -0300

    add: 1759.cs solution

---
+++ C#/Iniciante/1759.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        long x = long.Parse(Console.ReadLine());
+        
+        for(int i = 0; i<x-1; i++)
+        {
+            Console.Write("Ho ");
+        }
+        Console.WriteLine("Ho!");
+
+    }
+
+}
